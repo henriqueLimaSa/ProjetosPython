77a50488d644bb4ff6d7ae1920fb6dc8f48108d6
henrique lima <henriquebahia201996@gmail.com>
2023-09-13 21:00:57 -0300

    Adicionado o arquivo e a classe Extrato

--- Programas_POO/testecontas.py
+++ Programas_POO/testecontas.py
@@ -1,10 +1,10 @@
 from contas import Conta
 from clientes import Cliente
 
-cliente1 = Cliente(123, "Joao", "Rua 1")
-cliente2 = Cliente(345, "Maria", "Rua 2")
-conta1 = Conta([cliente1, cliente2], 1, 0)
-#conta1.gerarsaldo()
-conta1.depositar(1500)
-conta1.sacar(500)
-conta1.gerarsaldo()
+cliente1 = Cliente("123", "Joao", "Rua X")
+cliente2 = Cliente("456", "Maria", "Rua W")
+conta1 = Conta([cliente1, cliente2], 1, 2000)
+conta1.depositar(1000)
+conta1.sacar(1500)
+conta1.extrato.extrato(conta1.numero)
+Extrato: 1
